fb13aae45e66c3e20986de87b049b6faef09afd1
Misael10x <misabar12389@gmail.com>
2026-03-14 12:52:07 -0600

    version 1.0

--- app.py
+++ app.py
@@ -3,37 +3,29 @@ from bfs import buscar_solucion_BSF
 
 app = Flask(__name__)
 
+# Ruta principal (Frontend)
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+# API que resuelve el puzzle
 @app.route("/resolver", methods=["POST"])
 def resolver():
 
-    data = request.json
+    estado = request.json["estado"]
 
-    if not data or "estado" not in data:
-        return jsonify({"error": "Estado requerido"}), 400
-
-    estado = data["estado"]
-
-    try:
-        estado_inicial = list(map(int, estado.split(",")))
-    except:
-        return jsonify({"error": "Formato incorrecto. Usa ejemplo: 4,2,3,1"}), 400
-
-    solucion = [1, 2, 3, 4]
+    estado_inicial = list(map(int, estado.split(",")))
+    solucion = [1,2,3,4]
 
     nodo_solucion = buscar_solucion_BSF(estado_inicial, solucion)
 
     resultado = []
 
-    if nodo_solucion:
-
+    if nodo_solucion is not None:
         nodo = nodo_solucion
 
-        while nodo.get_padre() is not None:
+        while nodo.get_padre() != None:
             resultado.append(nodo.get_datos())
             nodo = nodo.get_padre()
 
@@ -43,5 +35,6 @@ def resolver():
     return jsonify(resultado)
 
 
+# Ejecutar local
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
